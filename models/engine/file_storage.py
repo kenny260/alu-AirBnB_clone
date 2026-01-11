94750c7f0822449ddbd96ddc7ae97d3ea36bb54b
kenny260 <n.binthia@alustudent.com>
2026-01-11 21:55:16 +0200

    56843

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,41 +1,40 @@
 #!/usr/bin/python3
-"""FileStorage engine module."""
+"""FileStorage module."""
 
 import json
 from models.base_model import BaseModel
-from models.user import User
 
 
 class FileStorage:
-    """Serializes instances to JSON and deserializes back."""
+    """Serializes instances to a JSON file and deserializes back."""
 
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Return dictionary of stored objects."""
-        return self.__objects
+        """Return the dictionary of objects."""
+        return FileStorage.__objects
 
     def new(self, obj):
         """Add new object to storage."""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Serialize objects to JSON file."""
-        with open(self.__file_path, "w", encoding="utf-8") as file:
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             json.dump(
-                {k: v.to_dict() for k, v in self.__objects.items()},
-                file
+                {k: v.to_dict() for k, v in FileStorage.__objects.items()},
+                f
             )
 
     def reload(self):
         """Deserialize JSON file to objects."""
         try:
-            with open(self.__file_path, "r", encoding="utf-8") as file:
-                data = json.load(file)
-                for obj in data.values():
-                    cls = eval(obj["__class__"])
-                    self.new(cls(**obj))
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+                objs = json.load(f)
+                for key, value in objs.items():
+                    obj = BaseModel(**value)
+                    FileStorage.__objects[key] = obj
         except FileNotFoundError:
             pass
